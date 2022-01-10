28f39b816d4c1cabb3a7d9fe039a123b2915d77c
rohanmushan <97397494+rohanmushan@users.noreply.github.com>
2022-01-10 09:38:26 +0530

    Delete AssignQ1.py

--- AssignQ1.py
+++
@@ -1,7 +0,0 @@
-# Reverse a String and print it on console "Python Skills"
-
-str = "Python Skills"
-print("Before reversing string: " + str)
-
-str = str[::-1]                             #Reversing String
-print("After reversing string: " + str)
