8be66d17f8d3f4e81b726e09c3fbf931e81c5f4a
rohanmushan <97397494+rohanmushan@users.noreply.github.com>
2022-01-10 09:38:52 +0530

    Delete AssignQ4.py

--- AssignQ4.py
+++
@@ -1,9 +0,0 @@
-# Write Program - convert a percentage to a fraction 
-# (To convert a percentage into fraction let say 30% to a fraction)
-
-per = 30
-
-frac = per / 100
-
-print ("Fraction of 30% is:")
-print(frac)
