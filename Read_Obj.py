0308c37ef0de4370ad55cf0c728b9cd5fc516d11
rohanmushan <97397494+rohanmushan@users.noreply.github.com>
2022-01-10 10:03:20 +0530

    Delete Read_Obj.py

--- Read_Obj.py
+++
@@ -1,8 +0,0 @@
-try:
-    f=open("rohan.txt",'r',encoding='utf-8')
-    print(f.read())
-except:
-     print("operation not perform....")
-finally:
-    f=open("rohan.txt",'r',encoding="utf-8")
-    f.close()
